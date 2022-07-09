e0084e1936d7bbaf1acb835dc4ab87837dbce995
Kaden Hansen <kadendeanhansen@gmail.com>
2022-07-09 11:33:12 -0500

    add get ad set accessors to model classes

--- models/menuItem.ts
+++ models/menuItem.ts
@@ -1,10 +1,28 @@
 import { ObjectId } from "mongodb"
 
 export default class MenuItem {
+    public get imageDescription(): string {
+        return this._imageDescription;
+    }
+    public set imageDescription(value: string) {
+        this._imageDescription = value;
+    }
+    public get itemName(): string {
+        return this._itemName;
+    }
+    public set itemName(value: string) {
+        this._itemName = value;
+    }
+    public get image(): URL {
+        return this._image;
+    }
+    public set image(value: URL) {
+        this._image = value;
+    }
     constructor(
-        public itemName: string, 
-        public image: URL, 
-        public imageDescription: string, 
+        private _itemName: string, 
+        private _image: URL, 
+        private _imageDescription: string, 
         public id?: ObjectId
         ) {}
 }
--- models/services.ts
+++ models/services.ts
@@ -1,10 +1,28 @@
 import { ObjectId } from "mongodb"
 
 export default class Service {
+    public get description(): string {
+        return this._description;
+    }
+    public set description(value: string) {
+        this._description = value;
+    }
+    public get image(): URL {
+        return this._image;
+    }
+    public set image(value: URL) {
+        this._image = value;
+    }
+    public get title(): string {
+        return this._title;
+    }
+    public set title(value: string) {
+        this._title = value;
+    }
     constructor(
-        public title: string, 
-        public image: URL, 
-        public description: string, 
+        private _title: string, 
+        private _image: URL, 
+        private _description: string, 
         public id?: ObjectId
         ) {}
 }
